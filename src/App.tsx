cba0f2d1cb461ffcc30f502d44dbd9c5fcace118
gmin <a9120a@gmail.com>
2021-06-20 21:20:20 +0900

    create canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import Canvas from './paintBoard'
 
 function App() {
   return (
-    <div className="App">
-      헬로
+    <div>
+      <h1>gpf
+      </h1>
+      <Canvas/>
     </div>
   );
 }

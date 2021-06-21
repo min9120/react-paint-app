8593d63f4776ec2010ea1331adbc099b8aa84af0
gmin <a9120a@gmail.com>
2021-06-22 00:21:38 +0900

    build drawing polygon

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -2,29 +2,28 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import './canvas.css'
 
-interface CanvasProps{
-  width:number;
-  height:number;
-}
-
 type Coordinates = {
   x:number;
   y:number;
 }
-function Canvas({width, height}:CanvasProps) {
+function Canvas() {
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [mousePosition, setMousePosition] = useState<Coordinates|undefined>(undefined)
   const [isDrawing, setIsDrawing] = useState(false);
+  let startPoint:Coordinates|undefined = undefined;
+  let endPoint:Coordinates|undefined = undefined;
+  
 
   const getCoordinates = (event:MouseEvent):Coordinates|undefined =>{
     if(!canvasRef.current){
       return;
     }
     const canvas:HTMLCanvasElement = canvasRef.current;
+    
     return {
-      x:event.pageX-canvas.offsetLeft,
-      y:event.pageY-canvas.offsetTop
+      x:event.pageX - canvas.offsetLeft,
+      y:event.pageY - canvas.offsetTop
     }
   }
 
@@ -39,23 +38,27 @@ function Canvas({width, height}:CanvasProps) {
       context.lineJoin = "round";
       context.lineWidth = 3;
 
+      
+
       context.beginPath();
       context.moveTo(originalMousePosition.x, originalMousePosition.y);
       context.lineTo(newMousePosition.x, newMousePosition.y);
       context.closePath();
       context.stroke();
-
     }
   }
   const startDraw = useCallback((event:MouseEvent)=>{
+    console.log('start')
     const coordinate = getCoordinates(event);
     if(coordinate){
       setIsDrawing(true);
       setMousePosition(coordinate);
+      startPoint =coordinate;
     }
   },[])
 
   const draw = useCallback((event:MouseEvent)=>{
+    console.log('drawing')
     event.preventDefault();
     event.stopPropagation();
     if(isDrawing){
@@ -66,6 +69,20 @@ function Canvas({width, height}:CanvasProps) {
       }
     }
   }, [isDrawing, mousePosition])
+
+  const endDraw = useCallback((event:MouseEvent)=>{
+    const coordinate = getCoordinates(event);
+    if(coordinate){
+      endPoint = coordinate;
+      console.log(endPoint?.y)
+      if(startPoint&&endPoint){
+        console.log(startPoint?.x, endPoint?.x)
+        drawLine(startPoint, endPoint)
+      }
+      setIsDrawing(false);
+    }
+  }, [])
+
   const exitDraw = useCallback(()=>{
     setIsDrawing(false);
   }, [])
@@ -78,35 +95,37 @@ function Canvas({width, height}:CanvasProps) {
 
     canvas.addEventListener('mousedown', startDraw);
     canvas.addEventListener('mousemove', draw);
-    canvas.addEventListener('mouseup', exitDraw);
+    canvas.addEventListener('mouseup', endDraw);
     canvas.addEventListener('mouseleave', exitDraw);
     return ()=>{
       canvas.removeEventListener('mousedown', startDraw);
     canvas.removeEventListener('mousemove', draw);
-    canvas.removeEventListener('mouseup', exitDraw);
+    canvas.removeEventListener('mouseup', endDraw);
     canvas.removeEventListener('mouseleave', exitDraw);
     }
-  }, [startDraw, draw, exitDraw])
+  }, [startDraw, draw, exitDraw, endDraw])
 
   return (
     <div className = 'Canvas-container'>
-    <div className = 'Cavas-deleteMode-btn'>
+    <div className = 'Canvas-deleteMode-btn'>
       <input type="checkbox" name ="mode"></input>
       <label htmlFor="mode">delete mode</label>
     </div>
     
-    <canvas  
-    onLoad={()=>{ }} 
-    ref={canvasRef}
-    width = {width}
-    height = {height}>
-  </canvas>
 
     <div className = 'Canvas-zoom-btn'>
     <button>+</button>
     <button>-</button>
-
+    <button onClick={()=>{
+      canvasRef.current?.getContext("2d")?.clearRect(0,0,800,800);
+    }}>delete</button>
     </div>
+    <canvas  
+    onLoad={()=>{ }} 
+    ref={canvasRef}
+    width = {800}
+    height = {800}>
+  </canvas>
     </div>
   );
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   return (
     <div className ='App'>
-      <Canvas width = {window.innerWidth} height = {window.innerHeight}/>
+      <Canvas/>
       <List/>
     </div>
   );
